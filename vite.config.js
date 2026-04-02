fc0656aa2aa401f44d67bb7a0646e01943c88354
Leo Ghignone <leo.ghignoe@gmail.com>
2026-04-02 16:53:43 +1100

    Use https for local dev too

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,11 @@ export default defineConfig({
     assetsDir: 'assets',
   },
   publicDir: 'public',
+  server: {
+    https: {
+      key: resolve(__dirname, 'localhost-key.pem'),
+      cert: resolve(__dirname, 'localhost-cert.pem')
+    },
+    host: true,  // Allow external access (useful for mobile testing)
+  },
 });
